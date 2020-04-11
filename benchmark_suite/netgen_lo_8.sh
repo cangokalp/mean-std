743c9a5cb0ec6c47d6d790eb859306a99548d212
cangokalp <cangokalp@utexas.edu>
2020-04-11 13:17:18 -0500

    ignored directory removed

--- benchmark_suite/netgen_lo_8.sh
+++
@@ -1,5 +0,0 @@
-# NETGEN-LO-8 instances (m = 8*n)
-
-#         seed   #         n   src   trg         m    costs  supply               caps
-
-echo "13502460  12      4096    64    64     32768  1 10000     640  0 0 100 100  1 1000" > netgen_lo_8_12a.min.param
